8ab255d670606c09f7d246c4d6048cfea27f43d1
Chance Swartz <chanceswartz@gmail.com>
2020-12-01 15:42:13 -0600

    Adding 4th event listener, the rick is coming up with ideas, don't ya know?

--- js/index.js
+++ js/index.js
@@ -7,6 +7,18 @@ Array.from(NavA).forEach(function(link){
     })
 })
 
+const logoHead = document.querySelector('.logo-heading');
+logoHead.addEventListener('mouseenter', function(event){
+    logoHead.innerText = 'FART BUS'
+});
+logoHead.addEventListener('mouseleave', function(event){
+    logoHead.innerText = 'Fun Bus'
+});
+
+const img = document.querySelectorAll('img')
+img.addEventListener('dblclick', function(event){
+    
+})
 
 // NavA.addEventListener('click', function(event){
 //     event.preventDefault();
