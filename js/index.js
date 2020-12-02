b44906912ff5e1e804b3f14eee8509888ea7e850
Chance Swartz <chanceswartz@gmail.com>
2020-12-01 18:44:20 -0600

    Setting up pull request

--- js/index.js
+++ js/index.js
@@ -15,10 +15,16 @@ logoHead.addEventListener('mouseleave', function(event){
     logoHead.innerText = 'Fun Bus'
 });
 
-const img = document.querySelectorAll('img')
-img.addEventListener('dblclick', function(event){
-    
-})
+const image = document.querySelectorAll('img')
+
+
+
+
+// image.addEventListener()
+// image.addEventListener('dblclick', function(event){
+//     image.style = '50%'
+//     //img.setAtrribute('href', 'https://post.greatist.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg')
+// })
 
 // NavA.addEventListener('click', function(event){
 //     event.preventDefault();
