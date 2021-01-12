a93d7afec6a431994045ef28a486b539ac94338f
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-12 15:16:02 -0600

    Added two more event listeners, dbl click and aux click

--- js/index.js
+++ js/index.js
@@ -8,3 +8,15 @@ const logoHeading = document.querySelector('.logo-heading');
         logoHeading.innerText = 'Fun Bus'
     })
 //JUST 8 MORE!
+const introH2 = document.querySelector('.intro h2')
+    introH2.addEventListener('dblclick', () => {
+        introH2.innerText = "'YOU BOYS EVER BEEN TO MEXICO?!'"
+        introH2.style.fontSize = '10rem';
+        introH2.style.color = 'teal'
+    })
+
+const introImg = document.querySelector('.intro img')
+    introImg.addEventListener('auxclick', () => {
+        introImg.setAttribute('src', "https://post.greatist.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg")
+    })
+//JUST 6 MORE!! also, load doesn't want to work
