dcda069d114291ba52df6aef59ca774d278897b6
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-12 14:47:44 -0600

    Added first two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+//CREATE 10 DIFFERERERERNT EVENT LISTENERS!
+const logoHeading = document.querySelector('.logo-heading');
+    logoHeading.addEventListener('mouseenter', () => {
+        logoHeading.innerText = 'OBVIOUS BUS JOKE'
+    })
+    logoHeading.addEventListener('mouseleave', () => {
+        logoHeading.innerText = 'Fun Bus'
+    })
+//JUST 8 MORE!
