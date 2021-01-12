2f0755df98ae2fcf36c147920d9a4b4539c8c16d
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-12 15:49:54 -0600

    added two more listeners, mouseover and mousemove

--- js/index.js
+++ js/index.js
@@ -20,3 +20,13 @@ const introImg = document.querySelector('.intro img')
         introImg.setAttribute('src', "https://post.greatist.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg")
     })
 //JUST 6 MORE!! also, load doesn't want to work
+const textContent = document.querySelector('.text-content')
+    const tCH2 = textContent.querySelector('h2')
+        tCH2.addEventListener('mouseover', () => {
+            tCH2.innerText = 'These Snozzberries Taste like Snozzberries'
+        })
+const tCPara = textContent.querySelector('p')
+    tCPara.addEventListener('mousemove', () => {
+        tCPara.innerText = "'Is a sandwich really a sandwich without butter?'"
+    })
+//Just 4 More, we got this!
